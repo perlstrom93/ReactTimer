a426393af16ae4dcac449881d70903c0f0f75eb4
Adam Perlstrom <Adam Perlstrom>
2017-05-27 10:47:19 +0100

    create tests for countdownForm

--- app/tests/components/clock.test.js
+++ app/tests/components/clock.test.js
@@ -10,33 +10,33 @@ describe('Clock', ()=>{
     it('should exist', ()=>{
         expect(Clock).toExist();
     });
-});
 
-describe('render', ()=>{
-    it('should render clock to output', ()=>{
-        var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>),
-            $el = $(ReactDOM.findDOMNode(clock)),
-            actualText = $el.find('.clock-text').text();
+    describe('render', ()=>{
+        it('should render clock to output', ()=>{
+            var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>),
+                $el = $(ReactDOM.findDOMNode(clock)),
+                actualText = $el.find('.clock-text').text();
 
-        expect(actualText).toBe('01:02');
+            expect(actualText).toBe('01:02');
+        });
     });
-});
 
-describe('formatSeconds', ()=>{
-    it('should format seconds', ()=>{
-        var clock = TestUtils.renderIntoDocument(<Clock/>),
-            seconds = 615,
-            expected = '10:15',
-            actual = clock.formatSeconds(seconds);
+    describe('formatSeconds', ()=>{
+        it('should format seconds', ()=>{
+            var clock = TestUtils.renderIntoDocument(<Clock/>),
+                seconds = 615,
+                expected = '10:15',
+                actual = clock.formatSeconds(seconds);
 
-            expect(actual).toBe(expected);
-    });
-    it('should format seconds/when mins and secs are less than 10', ()=>{
-        var clock = TestUtils.renderIntoDocument(<Clock/>),
-            seconds = 61,
-            expected = '01:01',
-            actual = clock.formatSeconds(seconds);
+                expect(actual).toBe(expected);
+        });
+        it('should format seconds/when mins and secs are less than 10', ()=>{
+            var clock = TestUtils.renderIntoDocument(<Clock/>),
+                seconds = 61,
+                expected = '01:01',
+                actual = clock.formatSeconds(seconds);
 
-            expect(actual).toBe(expected);
+                expect(actual).toBe(expected);
+        });
     });
 });
