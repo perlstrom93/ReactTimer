18bb617f9c19532cf17d3e6de394a405d8691cb8
Adam Perlstrom <Adam Perlstrom>
2017-05-27 13:29:09 +0100

    wire up control component to countdown

--- app/components/Controls.js
+++ app/components/Controls.js
@@ -2,7 +2,14 @@ var React = require('react');
 
 var Controls = React.createClass({
     propTypes: {
-        countdownStatus: React.PropTypes.string.isRequired
+        countdownStatus: React.PropTypes.string.isRequired,
+        onStatusChange: React.PropTypes.func.isRequired
+    },
+
+    onStatusChange: function(newStatus){
+        return ()=>{
+            this.props.onStatusChange(newStatus);
+        }
     },
 
     render: function(){
@@ -10,16 +17,20 @@ var Controls = React.createClass({
 
         var renderStartStopButton = ()=>{
             if(countdownStatus === 'started'){
-                return <button className="button secondary">Pause</button>
+                return <button className="button secondary expanded" onClick={this.onStatusChange('paused')}>Pause</button>
             } else if (countdownStatus === 'paused') {
-                return <button className="button primary">Start</button>
+                return <button className="button primary expanded" onClick={this.onStatusChange('started')}>Start</button>
             }
         };
 
         return(
             <div className="controls">
-                {renderStartStopButton()}
-                <button className="button alert hollow">Clear</button>
+                <div className="column medium-6">
+                    {renderStartStopButton()}
+                </div>
+                <div className="column medium-6">
+                    <button className="button alert hollow expanded" onClick={this.onStatusChange('stopped')}>Clear</button>
+                </div>
             </div>
         );
     }
